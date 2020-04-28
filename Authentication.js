52ac65ee1ce3340aec3b79a42a4a6e07e65827bc
bcalexis <alexisbellon@outlook.com>
2020-04-27 20:44:40 -0500

    ÂUpgrades Authentication.js

--- Authentication.js
+++ Authentication.js
@@ -1,32 +1,37 @@
-const btn = document.querySelector('button.nav-link');
+const btn = document.querySelector('input[value="Login"]');
 const form = document.querySelector('form.login-form');
 let skel = {name: "", lastname: "",email: "",password: ""};
 
+
+btn.addEventListener("click",(e) =>{
+    auth();
+})
+
 function auth(){
     const xhr = new XMLHttpRequest()
-    skel.email = document.querySelector('input[placeholder="username"]');
-    skel.password = document.querySelector('input[placeholder="password"]');
-    let corr = skel.email.value
-    // console.log(corr+" y "+skel.password.value);
-    console.log(skel.password.value);
-    xhr.open('GET',`http://localhost:3000/users?email=${corr}`)
-    // xhr.open('GET',`http://localhost:3000/users`)
+    skel.email = document.getElementById('usuarioLogIn').value;
+    skel.password = document.getElementById('usuarioPass').value
+    let corr = skel.email
+    xhr.open('GET',`http://localhost:3000/usuarios`)
     xhr.setRequestHeader("Content-Type","application/json")
-    xhr.setRequestHeader("charset","utf-8")
+    // xhr.setRequestHeader("charset","utf-8")
+    console.log(JSON.stringify(skel));
     xhr.send(JSON.stringify(skel));
     xhr.onload = ()=>{
         if(xhr.status != 200){
-            alert(`${xhr.status} Fallo registro de obtener ${JSON.stringify(form)}`);
+            alert(`${xhr.status} Error:  ${xhr.statusText}`);
         }else{
-            console.log('esto esta funcionando\n'+xhr.responseText);
+            console.log('El request fue exitoso');
             const jsn = JSON.parse(xhr.responseText);
             let jsn2 = jsn[0];
-            console.log(jsn2.email);
-            if (jsn2.email == corr  && jsn2.password == skel.password.value){console.log(`Este es BD: ${jsn2.email}\n Este es Ingreso: ${corr}`)};
+            if (jsn2.email == corr  && jsn2.password == skel.password){
+                console.log(`Este es BD: ${jsn2.email}\n Este es Ingreso: ${corr}`)
+                window.location.href = './profile.html';
+            }else{
+                alert('ERROR! Usuario o contraseña incorrectos')
+            }
         }
     }
 
 
 }
-
-btn.addEventListener('click',auth())
