c4fe98e9cad35629241aada6ffe89f083020976d
josemnr <maninari97@gmail.com>
2020-05-11 18:14:26 -0500

    Sources files

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const port = 3000;
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use("/",express.static(__dirname+"/public/home"))
+app.use("/users",express.static(__dirname+"public/css"))
 app.use("/users",express.static(__dirname+"/public/users"))
+
 
 app.listen(port, function(){
     console.log(`Listening http://localhost:${port}`);
