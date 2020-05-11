e268a31f13b80b800287282423c7ce9e02e07862
josemnr <maninari97@gmail.com>
2020-05-10 22:11:19 -0500

    Add User Backend

---
+++ db/User.js
@@ -0,0 +1,70 @@
+const mongoose = require('./mongodb-connect'); 
+
+let userSchema = mongoose.Schema({
+   name:{
+        type: String,
+        required: true
+   },
+   lastName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        required: true,
+    },
+    collegeMajor:{
+        type: String,
+        required: true,
+    }
+})
+
+let User = mongoose.model('users', userSchema);
+
+// User.find({}, (err, docs)=>{
+//     if(docs){
+//         console.log(docs)
+//     }
+
+//     if(err){
+//         console.log(err);
+//     }
+// })
+
+async function getUsersAsync(){
+    let docs = [];
+    try{
+        docs = await User.find({});
+        console.log(docs); 
+    }catch(error){
+        console.log("error", error)
+    }
+    return docs;
+}
+
+
+function createUser(user){
+    let userMongo = User(user);
+    
+    userMongo.save()
+    .then((resp)=> console.log(resp))
+    .catch((err)=> console.log("Ocurrió un error", err))    
+}
+
+let newUser = {name:"Test", lastName:"Rmz", email:"is703804@iteso.mx", password:"1234", role:"Professor", collegeMajor:"Ing. en sistemas"}
+
+// crearUsuario(newUser);
+
+User.createUser = createUser;
+User.getUsersAsync = getUsersAsync;
+
+module.exports = User;
