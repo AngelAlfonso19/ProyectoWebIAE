c1b9e97c8585f12c4529dc2355d507f47f317598
josemnr <maninari97@gmail.com>
2020-05-13 04:30:25 -0500

    Add assessmentDetail backend

--- public/assessmentDetail/EvaluacionesGraphs.js
+++ public/assessmentDetail/EvaluacionesGraphs.js
@@ -1,130 +1,84 @@
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://localhost:3000/IAE')
-// xhr.setRequestHeader("Content-Type", "application/json");
-// xhr.send();
-// xhr.onload = () =>{
-//     if(xhr.status != 200){
-//         alert(`${xhr.status} Fallo registro de obtener`)
-//     }
-//     else{
-        
-//         showQuestions(JSON.parse(xhr.responseText));
-//     }
-// }
+let asssigmentList
+let temporalAssessments
+let SubjectID
+getAssessment();
+getAssigments();
+getProfessors();
 
-// function showQuestions(data){
-//     var text = "";
-
-//    data.forEach(element => {
-//       text += userToHtml(element)
-//    });
-
-//     document.getElementById("questions").innerHTML = text;
-
-// }
-
-// function userToHtml(obj) {
-//     //console.log(obj);
-//     if (obj.typequestion == "n"){
-//         return `
-//         <div class="row">
-//                                 <div class="container-fluid">
-//                                     <div class="row">
-//                                         <div class="col mt-5 offset-1">
-//                                             <h3>${obj.id}. ${obj.question}</h3>
-//                                         </div>
-//                                     </div>
-//                                     <div class="row">
-//                                         <div class="col d-flex justify-content-center">
-//                                             <table class="graph">                                        
-//                                                 <caption>Bar Chart HTML From HTML Table</caption>
-//                                                 <thead>
-//                                                     <tr>
-//                                                         <th scope="col">Item</th>
-//                                                         <th scope="col">Percent</th>
-//                                                     </tr>
-//                                                 </thead>
-//                                                 <tbody>
-//                                                     <tr style="height:85%">
-//                                                         <th scope="row">0</th>
-//                                                         <td><span>85%</span></td>
-//                                                     </tr>
-//                                                     <tr style="height:23%">
-//                                                         <th scope="row">1</th>
-//                                                         <td><span>23%</span></td>
-//                                                     </tr>
-//                                                     <tr style="height:7%">
-//                                                         <th scope="row">2</th>
-//                                                         <td><span>7%</span></td>
-//                                                     </tr>
-//                                                     <tr style="height:38%">
-//                                                         <th scope="row">3</th>
-//                                                         <td><span>38%</span></td>
-//                                                     </tr>
-//                                                     <tr style="height:35%">
-//                                                         <th scope="row">4</th>
-//                                                         <td><span>35%</span></td>
-//                                                     </tr>
-//                                                     <tr style="height:35%">
-//                                                         <th scope="row">5</th>
-//                                                         <td><span>50%</span></td>
-//                                                     </tr>
-//                                                 </tbody>
-//                                             </table>  
-//                                         </div>                             
-//                                     </div>
-//                                 </div>
-//                             </div>
-//     `}
-//     else{
-//         return `
-//         <div class="row">
-//                                 <div class="col-11 mt-5 offset-1">
-//                                     <h3>${obj.id}. ${obj.question}</h3>
-//                                 </div>
-//                             </div>
-//                             <div class="row">
-//                                 <div class="col-10 offset-1 d-flex justify-content-center">
-//                                     <form class="col-12" name="registrationForm">                               
-//                                         <textarea class="col-12 inputText"></textarea>            
-//                                     </form>
-//                                 </div>              
-//                             </div>
-//     `
-//     }
-// }
-
-// let xhr2 = new XMLHttpRequest();
-// xhr2.open('GET', 'http://localhost:3000/asignaturas')
-// xhr2.setRequestHeader("Content-Type", "application/json");
-// xhr2.send();
-// xhr2.onload = () =>{ 
-//     if(xhr2.status != 200){
-//         alert(`${xhr2.status} Fallo registro de obtener`)
-//     }
-//     else{
-        
-//         calificacion(JSON.parse(xhr2.responseText));
-//     }
-// }
-
-// function calificacion(data){
-//     let teacher = document.getElementById('listTeachers').value
-//     let course = document.getElementById('listSubject').value
-//     console.log(data.IAE);
-    /*
-    data.IAE.forEach(ele, () =>{
-        console.log(ele);
-        if(ele.teacher == teacher && ele.title == course){
-            let answers = data.IAE
-            console.log(answers);
-            answers.forEach(e, () => {
-                console.log(e);
-            })
+function getAssessment(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/assessmentDetail')
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+    xhr.onload = () =>{
+        if(xhr.status != 200){
+            alert(`${xhr.status} Fallo registro de obtener`)
         }
-    })
-    */
-    
-//}
+        else{
+            temporalAssessments = (JSON.parse(xhr.responseText));
+            SubjectID = temporalAssessments[0].SubjectID
+            console.log(SubjectID)
+            // getRelatedAssignment(SubjectID)
+        }
+    }
+}
 
+function getAssigments(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/assignment')
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+    xhr.onload = () =>{
+        if(xhr.status != 200){
+            alert(`${xhr.status} Fallo registro de obtener`)
+        }
+        else{
+            assigmentsList = JSON.parse(xhr.responseText);
+            assigmentListToHTML(JSON.parse(xhr.responseText))
+        }
+    }
+}
 
+function getProfessors(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/users')
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+    xhr.onload = () =>{
+        if(xhr.status != 200){
+            alert(`${xhr.status} Fallo registro de obtener`)
+        }
+        else{
+            userList = JSON.parse(xhr.responseText);
+            console.log(userList)
+            professorsListToHTML(userList)
+        }
+    }
+}
+
+function professorsListToHTML(userList){
+    let professorsList = "";
+    userList.forEach(professsor => {
+        if(professsor.typo == 3){
+            console.log(professsor);
+            professorsList += professorToHTML(professsor)
+        }
+    });
+    console.log(professorsList);
+    document.getElementById("listTeachers").innerHTML = professorsList
+}
+
+function professorToHTML(professor){
+    return `                                                        
+    <option>${professor.name}</option>`;
+}
+
+//Assigments
+function assigmentListToHTML(assigmentsList){
+    document.getElementById("listSubject").innerHTML = assigmentsList.map(a => assigmentToHTML(a)).join('<br>')
+}
+
+function assigmentToHTML(assigment){
+    return `                                                        
+    <option>${assigment.SubjectName}</option>`;
+}
