004743798b5c8d6fb667bc1b0cbb9e42b27695a1
bcalexis <alexisbellon@outlook.com>
2020-05-17 03:03:57 -0500

    srvJS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser')
 // const cookieParser = require('cookie-parser')
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT ||  3000;
 
 app.use(bodyParser.json())
 // app.use(cookieParser());
